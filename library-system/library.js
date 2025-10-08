c1d8173c938c8750fe0da3c3d16a4719216b7dd8
Caroline Ho <caroline.cm.ho@gmail.com>
2025-10-08 11:40:37 -0400

    fixed everythign and using switch statement now

--- library-system/library.js
+++ library-system/library.js
@@ -4,11 +4,14 @@ import books from "./books.js";
 
 const prompt = promptSync();
 
-function addBook(title, author, year) {
+function addBook() {
+    let title = prompt('Title: ');
+    let author = prompt('Author: ');
+    let year = prompt('Year: ');
     const newBook = {
-        title: this.title,
-        author: this.author,
-        year: this.year,
+        title: title,
+        author: author,
+        year: year,
         isAvailable: true
     };
 
@@ -21,12 +24,13 @@ function listBooks() {
     for (let index in books) {
         let book = books[index];
         if (book.isAvailable) {
-            console.log(book[index].title);
+            console.log(book.title);
         };
     };
 };
 
-function borrowBook(title) {
+function borrowBook() {
+    let title = prompt('What book would you like to borrow? ');
     let book;
     for (let index in books) {
         if (books[index].title === title) {
@@ -42,7 +46,8 @@ function borrowBook(title) {
     };
 };
 
-function returnBook(title) {
+function returnBook() {
+    let title = prompt('What are you returning? ');
     let book;
     for (let index in books) {
         if (books[index].title === title) {
@@ -54,7 +59,8 @@ function returnBook(title) {
     console.log(`${title} is returned`);
 };
 
-function listBookAuthor(author) {
+function listBookAuthor() {
+    let author = prompt('What author? ');
     console.log(`Books by the author, ${author}`)
     for (let index in books) {
         let book = books[index];
@@ -64,7 +70,8 @@ function listBookAuthor(author) {
     };
 };
 
-function findBooksBeforeYear(year) {
+function findBooksBeforeYear() {
+    let year = prompt('What year? ');
     for (let index in books) {
         let book = books[index];
         if (book.year < year) {
@@ -73,49 +80,72 @@ function findBooksBeforeYear(year) {
     };
 };
 
-function removeBook(title) {
+function removeBook() {
+    let title = prompt('What book? ');
     for (let index in books) {
-        let book = books[index];
-        if (book.title === title) {
-            library.splice(index, index+1);
+        let b = books[index];
+        if (b.title === title) {
+            books.splice(index, 1);
             console.log(`${title} is removed`);
-        } else {
-            console.log(`${title} is not found in the library`);
         };
     };
+    console.log(`${title} is not found in the library`);
 };
 
 while (true) {
+    console.log('--------Library Options--------');
     console.log('1. Add a new book \n2. List available books \n3. Borrow a book \n4. Return a book \n5. List books by author \n6. Find books before a year \n7. Remove a book \n8. Exit program');
-    const num = console.log(prompt('What do you want to do?'));
-    
-    if (num === 1) {
-        let title = console.log(prompt('Title:'));
-        let author = console.log(prompt('Author:'));
-        let year = console.log(prompt('Year:'));
-        addBook(title, author, year);
-    } else if (num === 2) {
-        listBooks();
-    } else if (num === 3) {
-        let book = console.log(prompt('What book would you like to borrow?'));
-        borrowBook(book);
-    } else if (num === 4) {
-        let book = console.log(prompt('What are you returning?'));
-        returnBook(book);
-    } else if (num === 5) {
-        let author = console.log(prompt('What author?'));
-        listBookAuthor(author);
-    } else if (num === 6) {
-        let year = console.log(prompt('What year?'));
-        findBooksBeforeYear(year);
-    } else if (num === 7) {
-        let book = console.log(prompt('What book?'));
-        removeBook(book);
-    } else if (num === 7) {
-        console.log('BYEBYE!');
-        break;
-    } else {
-        console.log('Invalid input, try again stupid');
-    };
+    let num = prompt('What do you want to do? (1-8) ');
+
+    switch (num) {
+        case '1':
+            addBook();
+            break;
+        case '2':
+            listBooks();
+            break;
+        case '3':
+            borrowBook();
+            break;
+        case '4':
+            returnBook();
+            break;
+        case '5':
+            listBookAuthor();
+            break;
+        case '6':
+            findBooksBeforeYear();
+            break;
+        case '7':
+            removeBook();
+            break;
+        case '8':
+            console.log('BYEBYE!');
+            process.exit();
+        default:
+            console.log('Invalid input, try again');
+            break;
+    }
+
+    // if (num === '1') {
+    //     addBook();
+    // } else if (num === '2') {
+    //     listBooks();
+    // } else if (num === '3') {
+    //     borrowBook();
+    // } else if (num === '4') {
+    //     returnBook();
+    // } else if (num === '5') {
+    //     listBookAuthor();
+    // } else if (num === '6') {
+    //     findBooksBeforeYear();
+    // } else if (num === '7') {
+    //     removeBook();
+    // } else if (num === '8') {
+    //     console.log('BYEBYE!');
+    //     break;
+    // } else {
+    //     console.log('Invalid input, try again');
+    // };
 };
 
