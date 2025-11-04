92d9cf9530c7e1b5319bc7a807d6b287f7b90849
Caroline Ho <caroline.cm.ho@gmail.com>
2025-11-03 21:06:07 -0500

    finished homeworkgit add .

--- advanced_js/Homework.js
+++ advanced_js/Homework.js
@@ -16,6 +16,26 @@ const book = [
     {title: "Dune", isAvailable: true},
     {title: "Eloquent JavaScript", isAvailable: false},
     {title: "Fahrenheit 451", isAvailable: true}
+];
+const employee = [
+  {name: "Cindy", yearsAtCompany: 2.0},
+  {name: "Caitlin", yearsAtCompany: 1.0},
+  {name: "Daisy", yearsAtCompany: 0.1},
+  {name: "Ian", yearsAtCompany: 5.1},
+  {name: "Daniel", yearsAtCompany: 6},
+  {name: "Stefano", yearsAtCompany: 6},
+  {name: "Mr. DesLauriers", yearsAtCompany: 10}
+];
+const num2 = [
+  [1,2,3],
+  [4,5,6]
+]
+const movie = [
+  {title: "Inception", year: 2010},
+  {title: "The Matrix", year: 1999},
+  {title: "Interstellar", year: 2014},
+  {title: "The Godfather", year: 1972},
+  {title: "Pulp Fiction", year: 1994}
 ]
 
 //homework
@@ -32,7 +52,7 @@ const sum = num.reduce((acc, num) => acc + num, 0);
 console.log(sum);
 
 //4
-const greeting = name1.forEach(n => "Hello", n);
+const greeting = name1.forEach(n => "Hello" + n);
 console.log(greeting);
 
 // 5
@@ -49,5 +69,14 @@ const avail = book.every(book => book.isAvailable);
 console.log(avail);
 
 // 8
-const
+const moreThan5Years = employee.findIndex(employee => employee.yearsAtCompany > 5);
+console.log(moreThan5Years);
+
+// 9
+const flatten = num2.flatMap(num2 => num2*3);
+console.log(flatten);
+
+// 10
+const sort = movie.sort((a,b) => a.year-b.year);
+console.log(sort);
 
